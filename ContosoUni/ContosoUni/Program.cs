128af5488c94029562f5a8f078e57962e9a3d154
riinojassoo <riin.ojassoo@hotmail.com>
2024-08-27 10:40:11 +0300

    MigrationSetup

--- ContosoUni/ContosoUni/Program.cs
+++ ContosoUni/ContosoUni/Program.cs
@@ -1,3 +1,6 @@
+using ContosoUni.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace ContosoUni
 {
 	public class Program
@@ -5,6 +8,11 @@ namespace ContosoUni
 		public static void Main(string[] args)
 		{
 			var builder = WebApplication.CreateBuilder(args);
+
+			builder.Services.AddDbContext<SchoolContext>(options =>
+				options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+			builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 			// Add services to the container.
 			builder.Services.AddControllersWithViews();
