2e88bd3f74472a9b0fa2e21884e71d42f0260ee9
riinojassoo <riin.ojassoo@hotmail.com>
2024-08-27 11:30:13 +0300

    DbProgram

--- ContosoUni/ContosoUni/Program.cs
+++ ContosoUni/ContosoUni/Program.cs
@@ -19,6 +19,8 @@ namespace ContosoUni
 
 			var app = builder.Build();
 
+			CreateDbIfNotExists(app);
+
 			// Configure the HTTP request pipeline.
 			if (!app.Environment.IsDevelopment())
 			{
@@ -39,6 +41,26 @@ namespace ContosoUni
 				pattern: "{controller=Home}/{action=Index}/{id?}");
 
 			app.Run();
-		}
-	}
+
+            static void CreateDbIfNotExists(IHost host)
+            {
+                using(var scope = host.Services.CreateScope()) 
+				{
+					var services = scope.ServiceProvider;
+					try
+					{
+						var context = services.GetRequiredService<SchoolContext>();
+						DbInitializer.Initialize(context);
+					}
+					catch (Exception ex) 
+					{
+						var logger = services.GetRequiredService<ILogger<Program>>();
+						logger.LogError(ex, "An error occured creating the DB.");
+                    }
+                }
+            }
+        }
+    }
 }
+
+
