f4dc44caa7604f462952d887d008d3e96ca838f8
riinojassoo <riin.ojassoo@hotmail.com>
2024-08-27 09:51:38 +0300

    Model

---
+++ ContosoUni/ContosoUni/Models/Course.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUni.Models
+{
+    public class Course
+    { 
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
