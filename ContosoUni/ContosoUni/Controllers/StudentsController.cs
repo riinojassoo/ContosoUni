f188181869cc6561e1b0567f9f6036cee12d584a
riinojassoo <riin.ojassoo@hotmail.com>
2024-09-09 10:15:14 +0300

    StudentIndexAndDetails

---
+++ ContosoUni/ContosoUni/Controllers/StudentsController.cs
@@ -0,0 +1,47 @@
+﻿using ContosoUni.Data;
+using ContosoUni.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoUni.Controllers
+{
+	public class StudentsController : Controller
+	{
+		private readonly SchoolContext _context;
+
+		public StudentsController
+			(
+			SchoolContext context
+			)
+		{
+			_context = context;
+		}
+		public async Task<IActionResult> Index()
+		{
+			var result = await _context
+				.Students.ToListAsync();
+			return View(result);
+		}
+
+		public async Task<IActionResult> Details(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
+
+			var student = await _context.Students
+				.Include(s => s.Enrollments)
+				 .ThenInclude(e => e.Course)
+				.AsNoTracking()
+				.FirstOrDefaultAsync(m => m.ID == id);
+
+			if (student == null)
+			{
+				return NotFound();
+			}
+
+			return View(student);
+		}
+	}
+}
--- ContosoUni/ContosoUni/Program.cs
+++ ContosoUni/ContosoUni/Program.cs
@@ -14,10 +14,15 @@ namespace ContosoUni
 
 			builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-			// Add services to the container.
-			builder.Services.AddControllersWithViews();
 
-			var app = builder.Build();
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            //builder.Services.AddDbContext<SchoolContext>(options =>
+            //    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection)")));
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+            var app = builder.Build();
 
 			CreateDbIfNotExists(app);
 
