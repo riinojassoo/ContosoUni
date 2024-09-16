28bd7853ee3cd9ed8ead8d14a943468527ceba20
riinojassoo <riin.ojassoo@hotmail.com>
2024-09-16 11:31:44 +0300

    ComplexMigration

--- ContosoUni/ContosoUni/Models/Course.cs
+++ ContosoUni/ContosoUni/Models/Course.cs
@@ -1,12 +1,21 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUni.Models
 {
     public class Course
-    { 
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Display(Name = "Number")]
         public int CourseID { get; set; }
+
+        [StringLength(50, MinimumLength = 3)]
         public string Title { get; set; }
+
+        [Range(0, 5)]
         public int Credits { get; set; }
+
+        public int DepartmentId { get; set; }
 
         public ICollection<Enrollment> Enrollments { get; set; }
     }
---
+++ ContosoUni/ContosoUni/Models/Instructor.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUni.Models
+{
+	public class Instructor
+	{
+		public int ID { get; set; }
+
+		[Required]
+		[StringLength(50)]
+		[Display(Name = "Last Name")]
+		public string LastName { get; set; }
+
+		[Required]
+		[StringLength(50)]
+		[Column("FirstName")]
+		[Display(Name = "First Name")]
+		public string FirstMidName { get; set; }
+
+		[DataType(DataType.Date)]
+		[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+		[Display(Name = "Hire Date")]
+		public DateTime HireDate { get; set; }
+
+		[Display(Name = "Full Name")]
+		public string FullName
+		{
+			get
+			{
+				return LastName + ", " + FirstMidName;
+			}
+		}
+
+		public ICollection<CourseAssignment>? CourseAssignments { get; set; }
+		public OfficeAssignment OfficeAssignment { get; set; }
+	}
+}
