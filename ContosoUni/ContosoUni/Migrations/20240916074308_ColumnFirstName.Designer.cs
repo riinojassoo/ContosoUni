4df33e166c4ab2abe46e72d9045bf278224cab2f
riinojassoo <riin.ojassoo@hotmail.com>
2024-09-16 10:58:50 +0300

    StudentModelAdd

--- ContosoUni/ContosoUni/Migrations/20240916074308_ColumnFirstName.Designer.cs
+++ ContosoUni/ContosoUni/Migrations/20240916074308_ColumnFirstName.Designer.cs
@@ -4,6 +4,7 @@ using ContosoUni.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ContosoUni.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20240916074308_ColumnFirstName")]
+    partial class ColumnFirstName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
